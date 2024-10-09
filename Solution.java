658109322ac4c96d93f987cc8dbe699792f33cdf
Naveen Mathew Georgi <naveengeorgius@gmail.com>
2024-10-09 13:54:35 -0500

    Done

---
+++ Solution.java
@@ -0,0 +1,26 @@
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+public class Solution extends Relation {
+    public int findCelebrity(int n) {
+        int celeb = 0;
+        for (int i = 1; i < n; i++) {
+            if (knows(celeb, i)) {
+                celeb = i;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (i == celeb)
+                continue;
+            if (i < celeb) {
+                if (knows(celeb, i))
+                    return -1;
+            }
+            if (!knows(i, celeb))
+                return -1;
+        }
+
+        return celeb;
+    }
+}
